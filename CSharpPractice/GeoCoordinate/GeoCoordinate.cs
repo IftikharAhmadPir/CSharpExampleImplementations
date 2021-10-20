a10d0c998e678f445f9d0e6ca7cee12cb7983557
Iftikhar Ahmad <iftikharahmad.pir@gmail.com>
2021-10-20 20:32:22 +0200

    Changes I dont know about

--- CSharpPractice/GeoCoordinate/GeoCoordinate.cs
+++ CSharpPractice/GeoCoordinate/GeoCoordinate.cs
@@ -15,12 +15,12 @@ namespace CSharpPractice
             //point2.Longitude = Math.Round(point2.Longitude, 0) / 100000;
 
 
-            var d1 = point1.Latitude * (Math.PI / 180.0);
-            var num1 = point1.Longitude * (Math.PI / 180.0);
-            var d2 = point2.Latitude * (Math.PI / 180.0);
-            var num2 = point2.Longitude * (Math.PI / 180.0) - num1;
-            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
-            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+            var FromLat = point1.Latitude * (Math.PI / 180.0);
+            var FromLog = point1.Longitude * (Math.PI / 180.0);
+            var ToLat = point2.Latitude * (Math.PI / 180.0);
+            var ToLog = point2.Longitude * (Math.PI / 180.0) - FromLog;
+            var Converted = Math.Pow(Math.Sin((ToLat - FromLat) / 2.0), 2.0) + Math.Cos(FromLat) * Math.Cos(ToLat) * Math.Pow(Math.Sin(ToLog / 2.0), 2.0);
+            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(Converted), Math.Sqrt(1.0 - Converted)));
         }
     }
     public class Location
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -95,84 +95,84 @@ namespace CSharpPractice
             //    Longitude = 6.8759351
             //};
 
-            //var Point1 = new Location() { Latitude = 51.2419782, Longitude = 13.9158686 };
-            //var Point2 = new Location() { Latitude = 51.4410378, Longitude = 6.8759351 };
+            var Point1 = new Location() { Latitude = 51.2419782, Longitude = 13.9158686 };
+            var Point2 = new Location() { Latitude = 51.4410378, Longitude = 6.8759351 };
 
-            //var Point3 = new Location() { Latitude = 42.546245, Longitude = 1.601554 };
-            //var Point4 = new Location() { Latitude = 23.424076, Longitude = 53.847818 };
+            var Point3 = new Location() { Latitude = 42.546245, Longitude = 1.601554 };
+            var Point4 = new Location() { Latitude = 23.424076, Longitude = 53.847818 };
 
-            //var Point5 = new Location() { Latitude = 33.93911, Longitude = 67.709953 };
-            //var Point6 = new Location() { Latitude = 17.060816, Longitude = -61.796428 };
+            var Point5 = new Location() { Latitude = 33.93911, Longitude = 67.709953 };
+            var Point6 = new Location() { Latitude = 17.060816, Longitude = -61.796428 };
 
-            //var Point7 = new Location() { Latitude = 51.2419782, Longitude = 13.9158686 };
-            //var Point8 = new Location() { Latitude = 51.4410378, Longitude = 6.8759351 };
+            var Point7 = new Location() { Latitude = 51.2419782, Longitude = 13.9158686 };
+            var Point8 = new Location() { Latitude = 51.4410378, Longitude = 6.8759351 };
 
-            //var Point9 = new Location() { Latitude = 18.220554, Longitude = -63.068615 };
-            //var Point10 = new Location() { Latitude = 41.153332, Longitude = 20.168331 };
+            var Point9 = new Location() { Latitude = 18.220554, Longitude = -63.068615 };
+            var Point10 = new Location() { Latitude = 41.153332, Longitude = 20.168331 };
 
-            //var Point11 = new Location() { Latitude = 40.069099, Longitude = 45.038189 };
-            //var Point12 = new Location() { Latitude = 12.226079, Longitude = -69.060087 };
+            var Point11 = new Location() { Latitude = 40.069099, Longitude = 45.038189 };
+            var Point12 = new Location() { Latitude = 12.226079, Longitude = -69.060087 };
 
-            //var Point13 = new Location() { Latitude = -11.202692, Longitude = 17.873887 };
-            //var Point14 = new Location() { Latitude = -75.250973, Longitude = -0.071389 };
+            var Point13 = new Location() { Latitude = -11.202692, Longitude = 17.873887 };
+            var Point14 = new Location() { Latitude = -75.250973, Longitude = -0.071389 };
 
-            //var Point15 = new Location() { Latitude = -38.416097, Longitude = -63.616672 };
-            //var Point16 = new Location() { Latitude = -14.270972, Longitude = -170.132217 };
+            var Point15 = new Location() { Latitude = -38.416097, Longitude = -63.616672 };
+            var Point16 = new Location() { Latitude = -14.270972, Longitude = -170.132217 };
 
-            //var Point17 = new Location() { Latitude = 47.516231, Longitude = 14.550072 };
-            //var Point18 = new Location() { Latitude = -25.274398, Longitude = 133.775136 };
+            var Point17 = new Location() { Latitude = 47.516231, Longitude = 14.550072 };
+            var Point18 = new Location() { Latitude = -25.274398, Longitude = 133.775136 };
 
-            //var Point19 = new Location() { Latitude = 12.52111, Longitude = -69.968338 };
-            //var Point20 = new Location() { Latitude = 40.143105, Longitude = 47.576927 };
+            var Point19 = new Location() { Latitude = 12.52111, Longitude = -69.968338 };
+            var Point20 = new Location() { Latitude = 40.143105, Longitude = 47.576927 };
 
-            //var Point21 = new Location() { Latitude = 43.915886, Longitude = 17.679076 };
-            //var Point22 = new Location() { Latitude = 13.193887, Longitude = -59.543198 };
+            var Point21 = new Location() { Latitude = 43.915886, Longitude = 17.679076 };
+            var Point22 = new Location() { Latitude = 13.193887, Longitude = -59.543198 };
 
-            //var Point23 = new Location() { Latitude = 23.684994, Longitude = 90.356331 };
-            //var Point24 = new Location() { Latitude = 50.503887, Longitude = 4.469936 };
+            var Point23 = new Location() { Latitude = 23.684994, Longitude = 90.356331 };
+            var Point24 = new Location() { Latitude = 50.503887, Longitude = 4.469936 };
 
-            //var Point25 = new Location() { Latitude = 12.238333, Longitude = -1.561593 };
-            //var Point26 = new Location() { Latitude = 42.733883, Longitude = 25.48583 };
+            var Point25 = new Location() { Latitude = 12.238333, Longitude = -1.561593 };
+            var Point26 = new Location() { Latitude = 42.733883, Longitude = 25.48583 };
 
-            //var Point27 = new Location() { Latitude = 25.930414, Longitude = 50.637772 };
-            //var Point28 = new Location() { Latitude = -3.373056, Longitude = 29.918886 };
+            var Point27 = new Location() { Latitude = 25.930414, Longitude = 50.637772 };
+            var Point28 = new Location() { Latitude = -3.373056, Longitude = 29.918886 };
 
-            //var Point29 = new Location() { Latitude = 9.30769, Longitude = 2.315834 };
-            //var Point30 = new Location() { Latitude = 32.321384, Longitude = -64.75737 };
-
-
-            //var Point31 = new Location() { Latitude = 4.535277, Longitude = 114.727669 };
-            //var Point32 = new Location() { Latitude = -16.290154, Longitude = -63.588653 };
-
-            //var Point33 = new Location() { Latitude = -14.235004, Longitude = -51.92528 };
-            //var Point34 = new Location() { Latitude = 25.03428, Longitude = -77.39628 };
-
-            //var Point35 = new Location() { Latitude = 27.514162, Longitude = 90.433601 };
-            //var Point36 = new Location() { Latitude = -54.423199, Longitude = 3.413194 };
+            var Point29 = new Location() { Latitude = 9.30769, Longitude = 2.315834 };
+            var Point30 = new Location() { Latitude = 32.321384, Longitude = -64.75737 };
 
 
+            var Point31 = new Location() { Latitude = 4.535277, Longitude = 114.727669 };
+            var Point32 = new Location() { Latitude = -16.290154, Longitude = -63.588653 };
+
+            var Point33 = new Location() { Latitude = -14.235004, Longitude = -51.92528 };
+            var Point34 = new Location() { Latitude = 25.03428, Longitude = -77.39628 };
+
+            var Point35 = new Location() { Latitude = 27.514162, Longitude = 90.433601 };
+            var Point36 = new Location() { Latitude = -54.423199, Longitude = 3.413194 };
 
 
-            //var resultList = new List<double>();
 
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point1, Point2));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point3, Point4));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point5, Point6));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point7, Point8));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point9, Point10));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point11, Point12));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point13, Point14));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point15, Point16));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point17, Point18));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point19, Point20));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point21, Point22));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point23, Point24));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point25, Point26));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point27, Point28));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point29, Point30));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point31, Point32));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point33, Point34));
-            //resultList.Add(GeoCoordinate.CalculateDistance(Point35, Point36));
+
+            var resultList = new List<double>();
+
+            resultList.Add(GeoCoordinate.CalculateDistance(Point1, Point2));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point3, Point4));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point5, Point6));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point7, Point8));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point9, Point10));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point11, Point12));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point13, Point14));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point15, Point16));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point17, Point18));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point19, Point20));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point21, Point22));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point23, Point24));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point25, Point26));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point27, Point28));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point29, Point30));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point31, Point32));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point33, Point34));
+            resultList.Add(GeoCoordinate.CalculateDistance(Point35, Point36));
 
 
 
@@ -230,7 +230,7 @@ namespace CSharpPractice
 
             //var Point35 = new Coordinates() { Latitude = 27.514162, Longitude = 90.433601 };
             //var Point36 = new Coordinates() { Latitude = -54.423199, Longitude = 3.413194 };
-            
+
             //var resultList = new List<double>();
 
             //resultList.Add(CoordinatesDistanceExtensions.DistanceTo(Point1, Point2));
@@ -251,7 +251,7 @@ namespace CSharpPractice
             //resultList.Add(CoordinatesDistanceExtensions.DistanceTo(Point31, Point32));
             //resultList.Add(CoordinatesDistanceExtensions.DistanceTo(Point33, Point34));
             //resultList.Add(CoordinatesDistanceExtensions.DistanceTo(Point35, Point36));
-            
+
             //var result = GeoCoordinate.CalculateDistance(Point1, Point2);
 
             Console.ReadLine();
