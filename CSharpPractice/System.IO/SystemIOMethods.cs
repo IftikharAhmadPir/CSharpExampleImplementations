e947e94f75477e54247c5b2830f2917a74a5f39a
Iftikhar Ahmad <iftikharahmad.pir@gmail.com>
2021-10-22 02:36:31 +0200

    System.IO.Path implementation

---
+++ CSharpPractice/System.IO/SystemIOMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+namespace CSharpPractice
+{
+    class SystemIOMethods
+    {
+        public static void formatPath()
+        {
+            var ret1 = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetCallingAssembly().CodeBase).LocalPath), "LIS.NetSped.Core.CrystalReports.exe");
+        }
+    }
+}
